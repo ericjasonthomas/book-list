1340c29dbff2b7020d35922d0bdfdd804a914ecd
Eric Thomas <eric.thomas@jellyfish.net>
2018-10-03 20:07:50 -0400

    booklist search sort and bookcard test

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,27 @@
   <div id="app">
     <header>
       <div id="branding">
-        <h1>Books</h1>
+        <div class="container">
+          <h1>Books</h1>
+        </div>
       </div>
       <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/books">Books</router-link>
+        <div class="container">
+          <!-- <router-link to="/">Home</router-link> | -->
+          <router-link to="/">Books</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
       </div>
-      <router-view/>
     </header>
+    <router-view/>
+    <footer>
+      <div class="container">Books</div>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
-body {
-  margin: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-  border:1px solid #ccc;
-  margin: 0 auto;
-  padding: 0;
-}
-#branding {
-  padding:12px;
-}
-#nav {
-  padding: 20px;
-  background:#eee;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
+@import "styles/variables.scss";
+@import "styles/global.scss";
+@import "styles/linkUnderline.scss";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home-page">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+  <div class="page">
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 
